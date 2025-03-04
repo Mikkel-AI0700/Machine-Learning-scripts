3ddcdbdb5f63aea8ef6d64fce8115a6f5f582956
Mikkel-AI0700 <mikeruau0700@gmail.com>
2025-03-04 15:38:19 +0800

    New commit

---
+++ scr_scripts/eda.py
@@ -0,0 +1,63 @@
+from typing import *
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class InheritorClass:
+    def __init__ (
+        self,
+        main_dataset : Union[np.ndarray, pd.DataFrame],
+        x_variable : Union[str, List[str]],
+        y_variable : List[str],
+        legend_to_color : str,
+        **kwargs
+    ):
+        self.main_df = main_dataset
+        self.x_variable = x_variable
+        self.y_variable = y_variable
+        self.hue_class = legend_to_color
+        self.extra_kwargs = kwargs
+
+    def _calculate_subplot_amount (self, max_column_border=3):
+        temp_column_border = len(max_column_border)
+        number_of_rows = (tmep_column_border // max_column_border) + (tmep_column_border % max_column_border > 0)
+        return number_of_rows, max_column_border
+
+class RelationalPlots (InheritorClass):
+    def __init__ (self):
+        self.relplot_function_references = {
+            "line" : sns.lineplot,
+            "scatter" : sns.scatterplot
+        }
+
+    def determine_relational_plot (self, relational_plot_type : str):
+        rows, columns = self._calculate_subplot_amount()
+        figure, axes = plt.subplots(rows, columns, figsize=(26.5, 15.5))
+        axes = axes.flatten()
+
+    def determine_relational_plot (self, relational_plot_type : str):
+        if relational_plot_type in self.relplot_function_references.keys():
+            relational_plot_reference = self.relplot_function_references.get(relational_plot_type)
+            for axes_iteration, column_iteration in enumerate(self.y_variable):
+                relational_plot_reference(data=self.main_df, x=self.x_variable, y=column_iteration, hue=self.hue_class, **self.extra_kwargs)
+
+class DistributionalPlots (InheritorClass):
+    def __init__ (self):
+        self.displot_function_references = {
+            "hist" : sns.histplot,
+            "kde" : sns.kdeplot,
+            "ecdf" : sns.ecdfplot
+        }
+
+    def determine_distributional_plot (self, distributional_plot_type : str):
+        rows, columns = self._calculate_subplot_amount()
+        figure, axes = plt.subplots(rows, columns, figsize=(26.5, 15.5))
+        axes = axes.flatten()
+
+        if distributional_plot_type in self.displot_function_references:
+            distributional_plot_reference = self.displot_function_references.get(distributional_plot_type, -1)
+            for axes_iteration, column_iteration in enumerate(self.y_variable):
+                distributional_plot_reference(data=self.main_df, x=self.x_variable, y=column_iteration, hue=self.hue_class, **self.extra_kwargs)
+
---
+++ scr_scripts/random_search_script.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from typing import *
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import KFold, StratifiedKFold, GridSearchCV, RandomizedSearchCV
+
+class TrainUsingRandomSearch:
+    def __init__ (self, standard_randomsearch_parameters, n_iter=10000):
+        self.rs_global_instance = RandomizedSearchCV(n_iter=n_iter, **standard_randomsearch_parameters)
+        self.randomsearch_attributes = {}
+        self.model_prediction_results = {}
+
+    def _distribute_attributes_and_predictions (self, test_dataset_x, convert_cv_to_pandas):
+        # ----- Storing the attibrutes into a tuple to be looped -----
+        attributes_to_store = [
+            ("rs_best_estimator", self.rs_global_instance.best_estimator_), 
+            ("rs_best_score", self.rs_global_instance.best_score_), 
+            ("rs_best_params", self.rs_global_instance.best_params_),
+            ("rs_scorer_scores", self.rs_global_instance.scorer_),
+            ("rs_classes", self.rs_global_instance.classes_)
+        ]
+        model_predictions_to_store = [
+            ("model_base_predictions", self.rs_global_instance.predict(test_dataset_x)),
+            ("model_proba_predictions", self.rs_global_instance.predict_proba(test_dataset_x))
+        ]
+
+        if convert_cv_to_pandas:
+            attributes_to_store.append(("rs_cv_results", pd.DataFrame(self.rs_global_instance.cv_results_)))
+        else:
+            attributes_to_store.append(("rs_cv_results", self.rs_global_instance.cv_results_))
+
+        # ----- Looping over the attributes_to_store to store into the self.randomsearch_attributes -----
+        for attribute_tuple in attributes_to_store:
+            self.randomsearch_attributes.update({attribute_tuple[0] : attribute_tuple[1]})
+
+        # ----- Looping over model predictions array to store into self.model_predictions_to_store -----
+        for model_predictions_tuple in model_predictions_to_store:
+            self.model_prediction_results.update({model_predictions_tuple[0] : model_predictions_tuple[1]})
+
+    def start_randomsearch_training (self, train_dataset_x, train_dataset_y, test_dataset_x):
+        if not all(dataset == None for dataset in [train_dataset_x, train_dataset_y, test_dataset_x, test_dataset_y]): 
+            # ----- Creating instance of RandomizedSearchCV, fitting with self.standard_parameters -----
+            print("[+] Starting RandomizedSearchCV training")
+            self.rs_global_instance.fit(train_dataset_x, train_dataset_y)
+
+            # ----- Distributing attributes and model predictions in dictionaries -----
+            print("[+] Distributing attributes and model predictions into dictionaries...")
+            self._distribute_attributes_and_predictions(test_dataset_x)
+        else:
+            raise ValueError("[+] Error: One of the passed datasets is empty. Pass all datasets with values")
+
+    def reset_model_predictions (self, model_prediction_dictionary):
+        model_prediction_dictionary = self.model_prediction_results
+        return model_prediction_dictionary
