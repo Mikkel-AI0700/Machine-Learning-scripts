1ea5ae6dff9dca8b31721f600d94f72f26fad483
Mikkel-AI0700 <mikeruau0700@gmail.com>
2025-03-06 14:56:58 +0800

    Modified eda.py and created simple_cleaning.py

--- scr_scripts/eda.py
+++ scr_scripts/eda.py
@@ -7,18 +7,17 @@ import matplotlib.pyplot as plt
 
 class InheritorClass:
     def __init__ (
-        self,
-        main_dataset : Union[np.ndarray, pd.DataFrame],
-        x_variable : Union[str, List[str]],
-        y_variable : List[str],
-        legend_to_color : str,
-        **kwargs
-    ):
+        self, main_dataset : Union[np.ndarray, pd.DataFrame], x_variable : Union[str, List[str]], y_variable : List[str], legend_to_color : str, **kwargs):
         self.main_df = main_dataset
         self.x_variable = x_variable
         self.y_variable = y_variable
         self.hue_class = legend_to_color
         self.extra_kwargs = kwargs
+
+    def _import_required_dependencies (self):
+        required_dependencies = ["numpy", "pandas", "seaborn", "matplotlib.pyplot"]
+        import importlib
+
 
     def _calculate_subplot_amount (self, max_column_border=3):
         temp_column_border = len(max_column_border)
@@ -61,3 +60,21 @@ class DistributionalPlots (InheritorClass):
             for axes_iteration, column_iteration in enumerate(self.y_variable):
                 distributional_plot_reference(data=self.main_df, x=self.x_variable, y=column_iteration, hue=self.hue_class, **self.extra_kwargs)
 
+class CategoricalPlots (InheritorClass):
+    def __init__ (self):
+        self.catplot_function_references = {
+            "strip" : sns.stripplot,
+            "box" : sns.boxplot,
+            "bar" : sns.barplot,
+            "count" : sns.countplot
+        }
+
+    def determine_categorical_plot (self, categorical_plot_type):
+        rows, columns = self._calculate_subplot_amount()
+        figure, axes = plt.subplots(rows, columns, figsize=(26.5, 15.5))
+        axes = axes.flatten()
+
+        if categorical_plot_type in self.catplot_function_references.keys():
+            categorical_plot_reference = self.catplot_function_references.get(categorical_plot_type)
+            for axes_iteration, column_iteration in enumerate(self.y_variable):
+                categorical_plot_reference(data=self.main_df, x=self.x_variable, y=self.y_variable, hue=self.hue_class)
