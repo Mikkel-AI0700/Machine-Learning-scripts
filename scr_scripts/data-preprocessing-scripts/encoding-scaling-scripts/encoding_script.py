ac2e889332d7450da4abf246cf9cc8821f2892fc
Mikkel-AI0700 <mikeruau0700@gmail.com>
2025-03-18 21:24:23 +0800

    Refactored logic

--- scr_scripts/data-preprocessing-scripts/encoding-scaling-scripts/encoding_script.py
+++ scr_scripts/data-preprocessing-scripts/encoding-scaling-scripts/encoding_script.py
@@ -1,60 +1,73 @@
 
-# IMPORTANT: IMPORT THESE BEFORE RUNNING THE EncodeColumns CLASS!
+# WARNING: DO NOT COPY PASTE THE IMPORTS ABOVE CLASS. USE DEPENDENCY IMPORTER
 from typing import *
+import logging
 import numpy
 import pandas
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, TargetEncoder, LabelEncoder, LabelBinarizer
 
 class EncodeColumns (BaseEstimator, TransformerMixin):
-    def __init__ (self, columns_to_preprocess : List[str],  encoding_preprocessing_type : str,  encoder_instance_parameters : dict, numpy_output : bool, pandas_output : bool):
+    def __init__ (self, columns_to_preprocess: List[str], encoding_preprocessing_type: str, encoder_instance_parameters: dict, numpy_output: bool, pandas_output: bool):
         self.columns = columns_to_preprocess
         self.encoding_type = encoding_preprocessing_type
         self.encoder_parameters = encoder_instance_parameters
         self.numpy_output = numpy_output
         self.pandas_output = pandas_output
-        self.encoder_instances = {
-            "ohe" : OneHotEncoder,
-            "ordinal" : OrdinalEncoder,
-            "target" : TargetEncoder,
-            "label" : LabelEncoder,
-            "binarizer" : LabelBinarizer
-        } 
 
-    def _is_correct_datatype (self, check_dataset_datatype : bool, check_column_datatype : bool, dataset : Union[numpy.ndarray, pandas.DataFrame]):
-        valid_dataset_datatypes = (numpy.ndarray, pandas.Series, pandas.DataFrame)
-        valid_column_datatypes = (numpy.object_, numpy.str_, numpy.int8, "category", "string")
+    def _is_correct_datatype (self, check_dataset: bool, check_column: bool, dataset: Union[numpy.ndarray, pandas.DataFrame]):
+        dataset_datatypes = (numpy.ndarray, pandas.Series, pandas.DataFrame)
+        column_datatypes = (numpy.object_, numpy.str_, numpy.int8, "category", "string")
 
-        if check_dataset_datatype and isinstance(dataset, valid_dataset_datatypes):
+        if check_dataset and isinstance(dataset, dataset_datatypes):
             return True
         else:
             raise ValueError("[-] Error: Dataset argument is not numpy or pandas")
 
-        if check_column_datatype:
-            if isinstance(dataset, valid_dataset_datatypes[0]) and dataset.dtype in valid_column_datatypes:
+        if check_column:
+            if isinstance(dataset, dataset_datatypes[0]) and dataset.dtype in column_datatypes:
                 return True
-            elif isinstance(dataset, (valid_dataset_datatypes[1], valid_dataset_datatypes[2])) and dataset[self.columns].dtypes.isin(valid_column_datatypes).all():
+            elif isinstance(dataset, (dataset_datatypes[1], dataset_datatypes[2])) and dataset[self.columns].dtypes.isin(column_datatypes).all():
                 return True
             else:
                 raise ValueError("[-] Error: Either numpy or pandas dataset's dtype is not correct")
 
-    def _transform_dataset (self, retain_numpy_structure : bool, retain_pandas_structure : bool, encoder_instance : TransformerMixin, dataset : Union[numpy.ndarray, pandas.Series, pandas.DataFrame]):
-        if retain_numpy_structure and self._is_correct_datatype(check_column_datatype=True, dataset=dataset) and dataset.ndim > 1:
-            encoded_numpy_dataset = encoder_instance.fit_transform(dataset)
-            return encoded_numpy_dataset
+    def _transform_dataset (
+            self, 
+            retain_numpy: bool, 
+            retain_pandas: bool, 
+            encoder: TransformerMixin, 
+            dataset: Union[numpy.ndarray, pandas.Series, pandas.DataFrame]
+        ):
+        if retain_numpy and self._is_correct_datatype(check_column=True, dataset=dataset) and dataset.ndim > 1:
+            logging.info("[*] {} detected. Doing {} now.".format(encoder.__class__.__name__))
+            dataset = encoder.fit_transform(dataset)
+            return dataset
 
-        if retain_pandas_structure and self._is_correct_datatype(check_column_datatype=True, dataset=dataset):
-            if encoder_instance.__class__.__name__ == "OneHotEncoder":
-                temp_ohe_encoding = pandas.DataFrame(encoder_instance.fit_transform(dataset[self.columns]), dataset[self.columns].index, dataset[self.columns].columns)
-                encoded_dataset = pandas.concat([dataset.drop(self.columns, axis=1), temp_ohe_encoding], axis=1)
-                return encoded_dataset
+        if retain_pandas and self._is_correct_datatype(check_column=True, dataset=dataset):
+            if encoder.__class__.__name__ == "OneHotEncoder":
+                logging.info("[*] OneHotEncoder detected. Doing OneHotEncoding now")
+                temp_ohe_dataset = pandas.DataFrame(encoder.fit_transform(dataset[self.columns]), dataset[self.columns].index, dataset[self.columns].columns)
+                dataset = pandas.concat([dataset.drop(self.columns, axis=1), temp_ohe_dataset], axis=1)
+                return dataset
             else:
-                dataset[self.columns] = encoder_instance.fit_transform(dataset[self.columns])
+                logging.info("[*] {} detected. Doing {} now".format(encoder.__class__.__name__))
+                dataset[self.columns] = encoder.fit_transform(dataset[self.columns])
                 return dataset
 
     def fit_transform(self, X, y=None):
-        if self.encoding_type in self.encoder_instances.keys() and self._is_correct_datatype(check_dataset_datatype=True, dataset=X):
+        logging.basicConfig(level=logging.INFO)
+
+        encoder_instances = {
+            "ohe": OneHotEncoder(**self.encoder_parameters or {}),
+            "ordinal": OrdinalEncoder(**self.encoder_parameters or {}),
+            "target": TargetEncoder(**self.encoder_parameters or {}),
+            "label": LabelEncoder(**self.encoder_parameters or {}),
+            "binarizer": LabelBinarizer(**self.encoder_parameters or {})
+        }
+
+        if self.encoding_type in encoder_instances.keys() and self._is_correct_datatype(check_dataset=True, dataset=X):
+            logging.info("[*] Passing dataset and other parameters now to dataset...")
             encoded_dataset = self._transform_dataset(self.numpy_output, self.pandas_output, encoder_instances.get(self.encoding_type), X)
-            return encoded_dataset
         else:
             raise ValueError("[-] Error: Either encoding argument doesn't exist in the encoder instances or dataset argument contains wrong datatypes")
--- scr_scripts/dependency_importer/dependency-importer.py
+++ scr_scripts/dependency_importer/dependency-importer.py
@@ -1,4 +1,5 @@
 
+# WARNING: DO NOT COPY PASTE THE IMPORTS ABOVE CLASS. USE DEPENDENCY IMPORTER
 from typing import *
 import logging
 import importlib
@@ -6,30 +7,31 @@ import importlib
 class ImportRequiredDependencies:
     def __init__ (self):
         self.general_dependencies = {
-            "numpy" : "numpy",
-            "pandas" : "pandas",
-            "seaborn" : "seaborn",
-            "matplotlib" : "matplotlib.pyplot"
+            "logging": "logging",
+            "numpy": "numpy",
+            "pandas": "pandas",
+            "seaborn": "seaborn",
+            "matplotlib": "matplotlib.pyplot"
         }
         self.scaler_dependencies = {
-            "Normalizer" : "sklearn.preprocessing.Normalizer",
-            "StandardScaler" : "sklearn.preprocessing.StandardScaler",
-            "MinMaxScaler" : "sklearn.preprocessing.MinMaxScaler",
-            "MaxAbsScaler" : "sklearn.preprocessing.MaxAbsScaler"
+            "Normalizer": "sklearn.preprocessing.Normalizer",
+            "StandardScaler": "sklearn.preprocessing.StandardScaler",
+            "MinMaxScaler": "sklearn.preprocessing.MinMaxScaler",
+            "MaxAbsScaler": "sklearn.preprocessing.MaxAbsScaler"
         }
         self.encoder_dependencies = {
-            "OneHotEncoder" : "sklearn.preprocessing.OneHotEncoder",
-            "OrdinalEncoder" : "sklearn.preprocessing.OrdinalEncoder",
-            "TargetEncoder" : "sklearn.preprocessing.TargetEncoder",
-            "LabelEncoder" : "sklearn.preprocessing.LabelEncoder",
-            "LabelBinarizer" : "sklearn.preprocessing.LabelBinarizer",
+            "OneHotEncoder": "sklearn.preprocessing.OneHotEncoder",
+            "OrdinalEncoder": "sklearn.preprocessing.OrdinalEncoder",
+            "TargetEncoder": "sklearn.preprocessing.TargetEncoder",
+            "LabelEncoder": "sklearn.preprocessing.LabelEncoder",
+            "LabelBinarizer": "sklearn.preprocessing.LabelBinarizer",
         }
         self.tuner_dependencies = {
-            "GridSearch" : "sklearn.model_selection.GridSearchCV",
-            "RandomizedSearch" : "sklearn.model_selection.RandomizedSearchCV"
+            "GridSearch": "sklearn.model_selection.GridSearchCV",
+            "RandomizedSearch": "sklearn.model_selection.RandomizedSearchCV"
         }
 
-    def _import_dependencies (self, dependency_list : List[str], dependency_dictionary : dict):
+    def _import_dependencies (self, dependency_list: List[str], dependency_dictionary: dict):
         try:
             for dependency in [dependency_dictionary.get(dependency) for dependency in dependency_list]:
                 logging.info("[*] Importing: {}".format(dependency))
@@ -37,15 +39,18 @@ class ImportRequiredDependencies:
         except ImportError as non_existent_module:
             logging.warn("[!] Failed to import: {} | {}".format(dependency, non_existent_module))
 
-    def import_through_selection (self, dependency_type : str, required_dependencies : List[str]):
+    def import_through_selection (self, dependency_type: str, required_dependencies: List[str]):
+        logging.basicConfig(level=logging.INFO)
+
         dependency_map = {
-            "general" : self.general_dependencies,
-            "scaler" : self.scaler_dependencies,
-            "encoders" : self.encoder_dependencies,
-            "tuners" : self.tuner_dependencies
+            "general": self.general_dependencies,
+            "scaler": self.scaler_dependencies,
+            "encoders": self.encoder_dependencies,
+            "tuners": self.tuner_dependencies
         }
 
         if dependency_type in dependency_map.keys():
+            logging.info("[*] Passing the required dependencies to the importer...")
             self._import_dependencies(required_dependencies, dependency_map.get(dependency_type))
         else:
             logging.critical("[!] Critical: Argument does not exist in dependency_map keys. Argument: {}".format(dependency_type))
