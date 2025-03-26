a0e3bffb363bd0db8b0fc927ebf42f0d413a78b2
Mikkel-AI0700 <mikeruau0700@gmail.com>
2025-03-26 23:27:03 +0800

    Fixed the logic on certain parts of the code

--- scr_scripts/plot-visualizations/exploratory-data-analysis.py
+++ scr_scripts/plot-visualizations/exploratory-data-analysis.py
@@ -5,7 +5,7 @@ import logging
 import numpy
 import pandas
 import seaborn
-from matplotlib.pyplot import subplots, Axes
+import matplotlib.pyplot
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,13 +16,11 @@ class InheritorClass:
         dataset: Union[numpy.ndarray, pandas.DataFrame],
         x_vars: Union[str, List[str]],
         y_vars: Union[str, List[str]],
-        hue_color: str,
         **kwargs
     ):
         self.dataset = dataset
-        self.x_vars = x_vars
-        self.y_vars = y_vars
-        self.hue = hue_color
+        self.x_vars = x_vars if isinstance(x_vars, list) else [x_vars]
+        self.y_vars = y_vars if isinstance(y_vars, list) else [y_vars]
         self.extra_params = kwargs
 
     def _calculate_row_amount (self, column_border=3):
@@ -36,7 +34,9 @@ class InheritorClass:
         return figure, axes
 
     def _plot(self, axes: Axes, plot_method: Callable[..., Axes]):
-        if plot_method.__class__.__name__ in [method.__class__.__name__ for method in [seaborn.histplot, seaborn.kdeplot, seaborn.ecdfplot]]:
+        axes = axes.flatten()
+
+        if plot_method in [seaborn.histplot, seaborn.kdeplot, seaborn.ecdfplot]:
             for axes_iteration, column_iteration in enumerate(self.x_vars):
                 plot_method(data=self.dataset, x=column_iteration, ax=axes[axes_iteration], **self.extra_params)
         else:
@@ -49,10 +49,9 @@ class RelationalPlots (InheritorClass):
         dataset: Union[numpy.ndarray, pandas.DataFrame],
         x_vars: Union[str, List[str]],
         y_vars: Union[str, List[str]],
-        hue_color: str,
         **kwargs
     ):
-        super().__init__(dataset, x_vars, y_vars, hue_color, **kwargs)
+        super().__init__(dataset, x_vars, y_vars, **kwargs)
         self.relplot_methods = {
             "line": seaborn.lineplot,
             "scatter": seaborn.scatterplot
@@ -73,10 +72,9 @@ class DistributionalPlots (InheritorClass):
         dataset: Union[numpy.ndarray, pandas.DataFrame],
         x_vars: Union[str, List[str]],
         y_vars: Union[str, List[str]],
-        hue_color: str,
         **kwargs
     ):
-        super().__init__(dataset, x_vars, y_vars, hue_color, **kwargs)
+        super().__init__(dataset, x_vars, y_vars, **kwargs)
         self.displot_methods = {
             "hist": seaborn.histplot,
             "kde": seaborn.kdeplot,
@@ -99,10 +97,9 @@ class CategoricalPlots (InheritorClass):
         dataset: Union[numpy.ndarray, pandas.DataFrame],
         x_vars: Union[str, List[str]],
         y_vars: Union[str, List[str]],
-        hue_color: str,
         **kwargs
     ):
-        super().__init__(dataset, x_vars, y_vars, hue_color, **kwargs)
+        super().__init__(dataset, x_vars, y_vars, **kwargs)
         self.catplot_methods = {
             "strip": seaborn.stripplot,
             "swarm": seaborn.swarmplot,
