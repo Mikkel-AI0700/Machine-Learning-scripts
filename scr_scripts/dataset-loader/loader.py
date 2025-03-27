803291f462bad4300079a85217364e57d926e864
Mikkel-AI0700 <mikeruau0700@gmail.com>
2025-03-27 11:01:32 +0800

    New script commit

---
+++ scr_scripts/dataset-loader/loader.py
@@ -0,0 +1,51 @@
+
+# WARNING: DO NOT IMPORT THE MODULES ABOVE THE CLASS. USE DEPENDENCY IMPORTER
+
+from typing import List, Dict, Union
+import os
+import re
+import logging
+import pandas
+import ucimlrepo
+
+class LoadDataset:
+    def __init__ (self, uci_id: int, load_method: str, filesystem_path: str):
+        self.uci_id = uci_id
+        self.loader_method = load_method
+        self.fs_path = filesystem_path
+        self.loader_methods = {
+            "csv": pandas.read_csv,
+            "xlsx": pandas.read_excel,
+            "json": pandas.read_json,
+            "pickle": pandas.read_pickle,
+            "uci": ucimlrepo.fetch_ucirepo
+        }
+
+    def _get_loading_method (self):
+        if self.loader_method in self.loader_methods.keys():
+            return self.loader_methods.get(self.loader_method)
+
+    def _load_pandas (self):
+        loader = self._get_loading_method()
+        temp_dict = {}
+
+        if os.path.exists(self.fs_path):
+            dataset_list = [
+                ("main_df", loader(self.fs_path)), 
+                ("copy_df", loader(self.fs_path).copy()), 
+                ("numpy_df", loader(self.fs_path).to_numpy())
+            ]
+            for dataset_tuple in dataset_list:
+                temp_dict.update({dataset_tuple[0]: dataset_tuple[1]})
+            return temp_dict
+
+    def _load_uci (self):
+        pass
+
+    def load (self, load_uci: bool, load_pandas: bool):
+        if load_uci:
+            dataset_dictionary = self._load_uci()
+        if load_pandas:
+            dataset_dictionary = self._load_pandas()
+        return dataset_dictionary
+
